f5572807fc3d277e4f72268f4263d5e4f95bb945
asb1512 <asb1512@gmail.com>
2023-05-11 11:17:39 -0400

    central doc styling

--- css/app.ts
+++ css/app.ts
@@ -1,15 +1,45 @@
 import { createUseStyles } from 'react-jss';
-import { htmlFontSize, baselinePX } from './base';
+import { htmlFontSize, baselinePX, base } from './base';
+import colors from './colors';
+import { h1, h2, h3, h4, h5, body } from './type';
+import queries from './queries';
 
 export default createUseStyles({
   '@global': {
     'html, body': {
       margin: 0,
+      color: colors.yellow,
+    },
+    '*': {
+      boxSizing: 'border-box',
     },
     html: {
-      fontFamily: 'system-ui, Helvetical Neue, Arial, sans-serif',
+      fontFamily: 'system-ui, Helvetica Neue, Helvetica, Arial, sans-serif',
       fontSize: htmlFontSize,
-      lineHeight: baselinePX,
+      lineHeight: `${baselinePX}px`,
+      backgroundColor: colors.darkPurple,
+      [queries.m]: {
+        fontSize: htmlFontSize * 0.75,
+      },
+    },
+    h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    p: {
+      ...body,
+    },
+    ol: {
+      padding: `0 0 0 ${base()}`,
+      margin: `0 0 ${base()} 0`,
+    },
+    ul: {
+      padding: `0 0 0 ${base()}`,
+      margin: `0 0 ${base()} 0`,
+    },
+    li: {
+      ...body,
     },
   },
   app: {
