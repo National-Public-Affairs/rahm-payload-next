9e2c4b2dbb8cf106c3ee2babe532c6a40fd85b5e
asb1512 <asb1512@gmail.com>
2023-05-11 15:57:44 -0400

    added grid

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import React, { useEffect } from 'react';
 import type { AppProps } from 'next/app';
+import { ModalProvider } from '@faceless-ui/modal';
+import { GridProvider } from '@faceless-ui/css-grid';
+import { WindowInfoProvider } from '@faceless-ui/window-info';
+import Header from '../components/layout/Header';
 import useStyles from '../css/app';
+import zIndex from '../css/zIndex';
+import breakpoints from '../css/breakpoints';
+import { base } from '../css/base';
 
 const MyApp = ({ Component, pageProps }: AppProps): React.ReactElement => {
   const classes = useStyles();
@@ -14,9 +21,35 @@ const MyApp = ({ Component, pageProps }: AppProps): React.ReactElement => {
   }, []);
 
   return (
-    <div className={classes.app}>
-      <Component {...pageProps} />
-    </div>
+    <WindowInfoProvider breakpoints={breakpoints}>
+      <ModalProvider zIndex={zIndex.modal}>
+        <GridProvider
+          rowGap={{
+            s: base(2),
+            m: base(2),
+            l: base(2),
+            xl: base(2),
+          }}
+          colGap={{
+            s: base(2),
+            m: base(2),
+            l: base(2),
+            xl: base(2),
+          }}
+          cols={{
+            s: 8,
+            m: 8,
+            l: 12,
+            xl: 12,
+          }}
+        >
+          <div className={classes.app}>
+            <Header />
+            <Component {...pageProps} />
+          </div>
+        </GridProvider>
+      </ModalProvider>
+    </WindowInfoProvider>
   );
 };
 
