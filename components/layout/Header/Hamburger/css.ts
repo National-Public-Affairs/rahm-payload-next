72db659119278a83d4dcd87d753a415cb116aaba
asb1512 <asb1512@gmail.com>
2023-05-12 17:36:55 -0400

    building out menu hamburger icon

---
+++ components/layout/Header/Hamburger/css.ts
@@ -0,0 +1,17 @@
+import { createUseStyles } from 'react-jss';
+import colors from '../../../../css/colors';
+
+export default createUseStyles({
+  line1: {
+    stroke: colors.darkPurple,
+  },
+  line2: {
+    stroke: colors.darkPurple,
+  },
+  line3: {
+    stroke: colors.darkPurple,
+  },
+  line4: {
+    stroke: colors.darkPurple,
+  },
+});
