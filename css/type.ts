7be0647126bf30856c25fb452f9fc6bb46271056
asb1512 <asb1512@gmail.com>
2023-05-15 17:09:33 -0400

    changed font weights

--- css/type.ts
+++ css/type.ts
@@ -1,51 +1,57 @@
 import { base, htmlFontSize } from './base';
 import queries from './queries';
 
-const heading = {
-  fontWeight: 'normal',
+const headingWeight = {
+  fontWeight: '700',
+};
+
+const bodyWeight = {
+  fontWeight: '400',
 };
 
 export const h1 = {
-  ...heading,
+  ...headingWeight,
   fontSize: base(5),
   lineHeight: base(5),
   margin: `0 0 ${base(2)}`,
 };
 
 export const h2 = {
-  ...heading,
+  ...headingWeight,
   fontSize: base(4.25),
   lineHeight: base(5),
   margin: `${base(2)} 0`,
 };
 
 export const h3 = {
-  ...heading,
+  ...headingWeight,
   fontSize: base(3.5),
   lineHeight: base(4),
   margin: `${base(2)} 0 ${base(1.5)}`,
 };
 
 export const h4 = {
-  ...heading,
+  ...headingWeight,
   fontSize: base(2.25),
   lineHeight: base(2.5),
   margin: `${base(1.5)} 0 ${base()}`,
 };
 
 export const h5 = {
-  ...heading,
+  ...headingWeight,
   fontSize: base(1.625),
   lineHeight: base(2.25),
   margin: `${base(1)} 0 ${base(0.5)}`,
 };
 
 export const body = {
+  ...bodyWeight,
   fontSize: htmlFontSize,
   lineHeight: base(1.5),
 };
 
 export const largeBody = {
+  ...bodyWeight,
   fontSize: 22,
   lineHeight: base(2.25),
   margin: `${base(0.5)} 0`,
