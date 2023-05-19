e3c0f092395a1bec43f9627e8cb0c1f5d5209bf9
asb1512 <andrew@olympicmedia.com>
2023-05-19 16:23:56 -0400

    minimal hero styling

--- components/layout/PageHero/Minimal/css.ts
+++ components/layout/PageHero/Minimal/css.ts
@@ -1,6 +1,8 @@
 import { createUseStyles } from 'react-jss';
 import { label } from '../../../../css/type';
 import { fadeInUp } from '../css';
+import colors from '../../../../css/colors';
+import { base } from '../../../../css/base';
 
 export default createUseStyles({
   '@keyframes fade-in-up': {
@@ -21,6 +23,28 @@ export default createUseStyles({
     animationDelay: '200ms',
   },
   wrap: {
-    
+    position: 'relative',
+    zIndex: 1,
+    padding: `${base(5)} 0`,
+  },
+  bg: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    zIndex: -10,
+    backgroundColor: colors.yellow,
+    display: 'grid',
+    gridTemplate: '100% / 100%',
+  },
+  polygon: {
+    gridColumn: '1',
+    gridRow: '1',
+    justifySelf: 'center',
+    alignSelf: 'center',
+    zIndex: 20,
+    width: '80%',
+    height: '60%',
   },
 });
