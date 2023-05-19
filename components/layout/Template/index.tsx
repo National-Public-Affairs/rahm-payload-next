868fd1cb985f55e25bfedaee61ad6c3b4e8f196b
asb1512 <andrew@olympicmedia.com>
2023-05-19 01:41:53 -0400

    adde min height to page content

--- components/layout/Template/index.tsx
+++ components/layout/Template/index.tsx
@@ -2,20 +2,27 @@ import React, { Fragment } from 'react';
 import Footer from '../Footer';
 import { Type as FooterType } from '../../../globals/Footer';
 import { Type as SocialMediaType } from '../../../globals/SocialMedia';
+import useStyles from './css';
 
 type Props = {
   footer: FooterType;
   socialMedia: SocialMediaType;
 }
 
-const Template: React.FC<Props> = ({ children, footer, socialMedia }) => (
-  <Fragment>
-    {children}
-    <Footer
-      footer={footer}
-      socialMedia={socialMedia}
-    />
-  </Fragment>
-);
+const Template: React.FC<Props> = ({ children, footer, socialMedia }) => {
+  const classes = useStyles();
+
+  return (
+    <Fragment>
+      <div className={classes.wrapper}>
+        {children}
+      </div>
+      <Footer
+        footer={footer}
+        socialMedia={socialMedia}
+      />
+    </Fragment>
+  );
+};
 
 export default Template;
