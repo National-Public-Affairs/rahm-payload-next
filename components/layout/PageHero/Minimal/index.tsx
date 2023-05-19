062ed7e4bfe13ac8553e73b2be7c98fd8f8497f2
asb1512 <andrew@olympicmedia.com>
2023-05-19 16:24:14 -0400

    adjusting output of minimal hero with shape

--- components/layout/PageHero/Minimal/index.tsx
+++ components/layout/PageHero/Minimal/index.tsx
@@ -1,24 +1,33 @@
 import React from 'react';
+import color from 'color';
 import RichText from '../../../RichText';
 import GridContainer from '../../GridContainer';
+import PolygonTwo from '../../../graphics/Polygons/PolygonTwo';
 import useStyles from './css';
+import colors from '../../../../css/colors';
 
 type Props = {
-  content: unknown
-  title: string
+  content: unknown;
 }
 
-const MinimalPageHero: React.FC<Props> = ({ title, content }) => {
+const MinimalPageHero: React.FC<Props> = ({ content }) => {
   const classes = useStyles();
 
   return (
-    <GridContainer className={classes.wrap}>
-      <p className={classes.title}>{title}</p>
-      <RichText
-        className={classes.richText}
-        content={content}
-      />
-    </GridContainer>
+    <div className={classes.wrap}>
+      <GridContainer>
+        <RichText
+          className={classes.richText}
+          content={content}
+        />
+      </GridContainer>
+      <div className={classes.bg}>
+        <PolygonTwo
+          fillColor={color(colors.yellow).darken(0.1)}
+          className={classes.polygon}
+        />
+      </div>
+    </div>
   );
 };
 
