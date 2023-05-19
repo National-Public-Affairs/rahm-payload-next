772da46e7722c0f2c300f40a3e1b24dbc52f2be0
asb1512 <andrew@olympicmedia.com>
2023-05-19 01:22:18 -0400

    applied anime to background media

--- components/layout/PageHero/HeroWithMedia/css.ts
+++ components/layout/PageHero/HeroWithMedia/css.ts
@@ -1,14 +1,22 @@
 import { createUseStyles } from 'react-jss';
 import { base } from '../../../../css/base';
 import queries from '../../../../css/queries';
-import { fadeInUp } from '../css';
+import { fadeInUp, twistFadeIn } from '../css';
 
 export default createUseStyles({
   '@keyframes fade-in-up': {
     ...fadeInUp,
   },
+  '@keyframes twist-fade-in': {
+    ...twistFadeIn,
+  },
   wrap: {
     margin: 0,
+    position: 'relative',
+    overflow: 'hidden',
+  },
+  gridContainer: {
+    zIndex: 1,
   },
   afterLoad: {},
   richText: {
@@ -41,5 +49,17 @@ export default createUseStyles({
   },
   gridElement: {
     alignSelf: 'end',
+  },
+  backgroundMedia: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    width: '75%',
+    zIndex: -10,
+    opacity: 0.2,
+    animationName: '$twist-fade-in',
+    animationDuration: '800ms',
+    animationTimingFunction: 'ease',
+    animationFillMode: 'both',
   },
 });
