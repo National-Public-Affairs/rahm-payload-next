cdaff4897c263d1bcde303aa6003ddcbcd135808
asb1512 <andrew@olympicmedia.com>
2023-05-19 18:57:35 -0400

    fixed naming

--- blocks/index.ts
+++ blocks/index.ts
@@ -1,9 +1,11 @@
 import { Component as cta } from './CallToAction/Component';
 import { Component as content } from './Content/Component';
 import { Component as image } from './Image/Component';
+import { Component as minimalHeroBlock } from './MinimalHero/Component';
 
 export const components = {
   cta,
   content,
   image,
+  minimalHeroBlock,
 };
