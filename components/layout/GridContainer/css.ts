bab25638fc9053ad3f95524cf2e678a09e4bae0f
asb1512 <andrew@olympicmedia.com>
2023-05-19 01:46:54 -0400

    added breakpoint styling

--- components/layout/GridContainer/css.ts
+++ components/layout/GridContainer/css.ts
@@ -1,10 +1,18 @@
 import { createUseStyles } from 'react-jss';
 import { gridContainerWidth } from '../../../css/structure';
+import queries from '../../../css/queries';
+import { base } from '../../../css/base';
 
 export default createUseStyles({
   gridContainer: {
     maxWidth: gridContainerWidth,
     marginLeft: 'auto',
     marginRight: 'auto',
+    [queries.l]: {
+      padding: base(1.5),
+    },
+    [queries.m]: {
+      padding: base(2),
+    },
   },
 });
