79a9dfbc4fc0624d8346a9a5b2098132c0655fed
asb1512 <andrew@olympicmedia.com>
2023-05-19 22:26:34 -0400

    created box shadows

---
+++ css/boxShadows.ts
@@ -0,0 +1,3 @@
+export default {
+  standard: '0px 0px 1.9px rgba(0, 0, 0, 0.028), 0px 0px 4.6px rgba(0, 0, 0, 0.04), 0px 0px 8.6px rgba(0, 0, 0, 0.05), 0px 0px 15.4px rgba(0, 0, 0, 0.06), 0px 0px 28.8px rgba(0, 0, 0, 0.072), 0px 0px 69px rgba(0, 0, 0, 0.1)',
+};
