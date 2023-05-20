d07fe48056411635871517b60f79a3f7977b3894
asb1512 <andrew@olympicmedia.com>
2023-05-19 23:53:34 -0400

    receives bg color as prop

--- blocks/Content/Component.tsx
+++ blocks/Content/Component.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import RichText from '../../components/RichText';
-import classes from './index.module.css';
+import useStyles from './css';
 
 export type Type = {
   blockType: 'content';
   blockName?: string;
+  backgroundColor: 'none' | 'purple' | 'darkPurple' | 'yellow' | 'white';
   content: unknown;
 }
 
-export const Component: React.FC<Type> = (props) => {
-  const { content } = props;
+export const Component: React.FC<Type> = ({
+  content,
+  backgroundColor,
+}) => {
+  const classes = useStyles({ backgroundColor });
 
   return (
     <div className={classes.wrap}>
