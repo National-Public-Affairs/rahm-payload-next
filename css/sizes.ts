662d4fae6e175baf4b9c770869018e99c41e0297
asb1512 <asb1512@gmail.com>
2023-05-22 16:20:33 -0400

    added max width property

--- css/sizes.ts
+++ css/sizes.ts
@@ -1,4 +1,5 @@
 import { base } from './base';
+import breakpoints from './breakpoints';
 
 export const strokeWidth = '3px';
 
@@ -7,3 +8,5 @@ export const headerHeight = base(8);
 export const small = base(2);
 export const medium = base(7);
 export const large = base(10);
+
+export const maxWidth = breakpoints.xxl;
